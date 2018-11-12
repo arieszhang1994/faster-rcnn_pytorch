d78c3550f3f4203ff9c864f6ad34ff9fe5e232f0
arieszhang <zhanghaoyu1994@yahoo.com>
2018-11-12 19:45:06 +0900

    fix pspooling for pytorch0.40

--- lib/model/psroi_pooling/functions/psroi_pooling.py
+++ lib/model/psroi_pooling/functions/psroi_pooling.py
@@ -4,40 +4,33 @@ from .._ext import psroi_pooling
 
 
 class PSRoIPoolingFunction(Function):
-    def __init__(self, pooled_height, pooled_width, spatial_scale, group_size, output_dim):
-        self.pooled_width = int(pooled_width)
-        self.pooled_height = int(pooled_height)
-        self.spatial_scale = float(spatial_scale)
-        self.group_size = int(group_size)
-        self.output_dim = int(output_dim)
-        self.output = None
-        self.mappingchannel = None
-        self.rois = None
-        self.feature_size = None
+    def __init__(ctx, pooled_height, pooled_width, spatial_scale, group_size, output_dim):
+        ctx.pooled_width = int(pooled_width)
+        ctx.pooled_height = int(pooled_height)
+        ctx.spatial_scale = float(spatial_scale)
+        ctx.group_size = int(group_size)
+        ctx.output_dim = int(output_dim)
+        ctx.mappingchannel = None
+        ctx.rois = None
+        ctx.feature_size = None
 
-    def forward(self, features, rois):
-        batch_size, num_channels, data_height, data_width = features.size()
-        num_rois = rois.size()[0]
-        output = torch.zeros(num_rois, self.output_dim, self.pooled_height, self.pooled_width)
-        mappingchannel = torch.IntTensor(num_rois, self.output_dim, self.pooled_height, self.pooled_width).zero_()
-        output = output.cuda()
-        mappingchannel = mappingchannel.cuda()
-        psroi_pooling.psroi_pooling_forward_cuda(self.pooled_height, self.pooled_width, self.spatial_scale, self.group_size, self.output_dim, \
-        features, rois, output, mappingchannel)
-        self.output = output
-        self.mappingchannel = mappingchannel
-        self.rois = rois
-        self.feature_size = features.size()
+    def forward(ctx, features, rois):
+        ctx.feature_size = features.size()
+        batch_size, num_channels, data_height, data_width = ctx.feature_size
+        num_rois = rois.size(0)
+        output = features.new(num_rois, ctx.output_dim, ctx.pooled_height, ctx.pooled_width).zero_()
+        ctx.mappingchannel =  features.new(num_rois, ctx.output_dim, ctx.pooled_height, ctx.pooled_width).zero_().int()
+        ctx.rois = rois
+        psroi_pooling.psroi_pooling_forward_cuda(ctx.pooled_height, ctx.pooled_width, ctx.spatial_scale, ctx.group_size, ctx.output_dim, \
+        features, rois, output, ctx.mappingchannel)
 
         return output
 
-    def backward(self, grad_output):
-        assert(self.feature_size is not None and grad_output.is_cuda)
+    def backward(ctx, grad_output):
+        assert(ctx.feature_size is not None and grad_output.is_cuda)
+        batch_size, num_channels, data_height, data_width = ctx.feature_size
+        grad_input = grad_output.new(batch_size, num_channels, data_height, data_width).zero_()
 
-        batch_size, num_channels, data_height, data_width = self.feature_size
-
-        grad_input = torch.zeros(batch_size, num_channels, data_height, data_width).cuda()
-
-        psroi_pooling.psroi_pooling_backward_cuda(self.pooled_height, self.pooled_width, self.spatial_scale, self.output_dim,  \
-        grad_output, self.rois, grad_input, self.mappingchannel)
+        psroi_pooling.psroi_pooling_backward_cuda(ctx.pooled_height, ctx.pooled_width, ctx.spatial_scale, ctx.output_dim,  \
+        grad_output, ctx.rois, grad_input, ctx.mappingchannel)
         return grad_input, None
